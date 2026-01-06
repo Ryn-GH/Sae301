d90d537ac6ae2b4466acefee52a099fc276ed0a3
lbiasini <luca.biasini91@gmail.com>
2026-01-06 11:17:23 +0100

    on retente
    ..

---
+++ erddap-api/storage/framework/views/cbb8c45c17571a2fe0bf08a6eb8d95f0.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "w-full max-w-7xl mx-auto p-4 sm:p-14 border-x border-dashed border-neutral-300 dark:border-white/[9%]"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH C:\Users\lucab\OneDrive - UPEC\Documents\BUT-INFO\BUT2\SAE3.01\Jalon4\erddap-api\vendor\laravel\framework\src\Illuminate\Foundation\Providers/../resources/exceptions/renderer/components/section-container.blade.php ENDPATH**/ ?>
